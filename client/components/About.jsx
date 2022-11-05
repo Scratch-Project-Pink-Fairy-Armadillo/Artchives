3e1f0d2bf603e26571a2ed85c43de6243cd182d2
Joe Hynan <joehynan@joes-air.lan>
2022-11-05 15:08:06 -0500

    api updates

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 
 // non-stateful component that will be added to app on home page
 export default function About() {
+
+  
+
   return(
     <article>
 
--- client/components/ArtBox.jsx
+++ client/components/ArtBox.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 export default function ArtBox() {
 
+    const url = `https://api.artic.edu/api/v1/artworks/${this.props.artId}?fields=id,title,image_id`
+
+    useEffect(() => {
+        fetch(url)
+        .then((data) => data.json())
+        .then((data) => {
+            
+      });
+    })
+
+    return(
+        <div className='artbox-div'>
+
+        </div>
+    )
 }
 
 
