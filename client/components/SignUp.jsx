1c2d11188b136c8478c39aa727e673e2f3aa0b0b
Cedar Cooper <cedar.m.cooper@gmail.com>
2022-11-05 15:52:44 -0700

    removed wrong wording

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -36,6 +36,11 @@ export default function SignUp() {
     })
   })
 }
+
+// want me to do sessions and set it
+// cooke event handler, set cookie with res, the res has to match debbie's frontend cookie
+
+
     // onsubmit
     // here we need logic for if the name and password already exist
     // do the post request and if "user" not found then send error back to client
@@ -78,33 +83,3 @@ export default function SignUp() {
   )
 }
 
- // onsubmit
-    // here we need logic for if the name and password already exist
-    // do the post request and if "user" not found then send error back to client
-        // check database for user, if not continue as normal
-    // if so, tell client so and have them login
-    // this needs to post to db and create a new user
-    // login then will need to check from there if these values exist
-    // we then need seperate handling for the bcrypt validation for login
-        
-    // login
-    // your actually making a post request to send body 
-    // use body to check name and passeord
-    // before loading favs
-
-
-    //favs
-    // get favs into state
-    // the component to make feth from 
-    // on succesful login, response could be uer favs and we would need to pass that up
-    // ot parent compoment that will be rendered in dom, successful login could send a cookie to validate
-    // who they are 
-// ideally db, so they can they get favs from wherever
-    // localstorage??
-    //OR when home page renders make a get req but thst has to validate like with sessions/jwts
-
-    //front end cookies, may need to be set to null at start
-
-    // will we need to have a combo signup / login?
-    // enter the info below yeah
-    // then first check that 
