b2bc1925cd74c3a1ede18c42575d23583e933b91
Joe Hynan <joehynan@joes-air.lan>
2022-11-06 00:09:29 -0500

    11/5

--- client/components/ArtBox.jsx
+++ client/components/ArtBox.jsx
@@ -4,26 +4,28 @@ import ArtContainer from './ArtContainer';
 
 
 export default function ArtBox(props) {
-    const [artTitle , setArtTitle] = useState(null);
-    const apiUrl = `https://api.artic.edu/api/v1/artworks/${props.artId}?fields=id,title,image_id`
+    console.log(props)
+    const [isFavorite , setFavorite] = useState(false);
 
 
-
+/*
     useEffect(() => {
+        console.log('here')
         fetch(apiUrl)
         .then((data) => data.json())
         .then((data) => {
-        const imgUrl = data.config.iiif_url + '/' + data.data.image_id + '/full/843,/0/default.jpg';
         setArtTitle(data.data.title);
-
-
+        setimgUrl(data.config.iiif_url + '/' + data.data.image_id + '/full/843,/0/default.jpg')
+        console.log(imgUrl);
       });
     }, []);
 
+*/
+
     return(
         <div id={props.artID} className='artbox-div'>
-            <img id={props.artID + '-image'}src={imgUrl} alt="artwork" />
-            <p>{artTitle}</p>
+            <img id={props.artID + '-image'} src={props.imgUrl} alt="artwork" />
+            <p>{props.title}</p>
             <button id={props.artID + '-button'} className='favorite-button'>Favorite</button>
         </div>
     )
--- client/components/ArtContainer.jsx
+++ client/components/ArtContainer.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ArtBox from './ArtBox';
 
 export default function ArtContainer() {
-    const idArray = ['229889', '239462', '155959', '140595', '146941']
-    const boxes = [];
-    for (let i = 0; i < 5; i++){
-        boxes.push(<ArtBox artid = {idArray[i]} />)
-    };
+    console.log('line 5');
 
-    return(
-      <div className='art-container'>{boxes}</div>
+    const [count, setCount] = useState(0);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+      const idArray = ['229889', '239462', '155959', '140595', '146941']
+      if (count<5){
+        const newCount = count + 1 
+        setCount(newCount);
+        createArtBox(idArray[count])
+      }
+
+    })
+
+    function createArtBox(id) {
+      console.log('line 22');
+      const apiUrl = 'https://api.artic.edu/api/v1/artworks/' + id + '?fields=id,title,image_id';
+
+      fetch(apiUrl)
+      .then((data) => data.json())
+      .then((data) => {
+        const artID = id;
+        const title = data.data.title;
+        const imgUrl = data.config.iiif_url + '/' + data.data.image_id + '/full/843,/0/default.jpg'
+        console.log(results);
+        const newResults = results.push((<ArtBox title = {title} imgUrl = {imgUrl} artID={id}/>))
+        setResults(newResults);
+    })
+    }
+
+    return (
+      <div className='art-container'> {results} </div>
     )
+
   }
-  
